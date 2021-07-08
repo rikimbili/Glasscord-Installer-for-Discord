a380801cc3e92fd734659f3e0e362a91ca814b98
Raciel <39206666+Rikimbili@users.noreply.github.com>
2021-07-08 01:07:10 -0400

    Initial WIP version, many features like checking right app version folder and modifying json file still to be implemented. Basic functions integrated to this version

--- Glasscord-Installer-for-Discord.cpp
+++ Glasscord-Installer-for-Discord.cpp
@@ -1,18 +1,115 @@
+/*
+ * TODO:
+ * 
+ *
+ */ 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
-using namespace std;
+#include <fstream>
+#include <string>
+#include <Windows.h>
+#include <filesystem>
+#include <iomanip>    
+#include <ctime>       
+#include <chrono>       
+#include "concol.h"
+
+#pragma comment(lib, "urlmon.lib")
+
+void GetDiscordPath(std::string& app_path); // Gets discord app path if it exists
+int IsDiscordInstalled(std::string app_path); // Returns 1 if Discord Canary is installed, returns 0 otherwise
+void GlasscordDownload(std::string app_path); // Download glasscord.asar to discordcanary app directory
+void log(std::string, int message_lvl); //Log message in console with a message level > Level 0: Error; Level 1: Warning; Level 2: Info
 
 int main()
 {
-    
+    eku::concolinit();
+    SetConsoleTitle(L"Glasscord Installer for Discord by Rikimbili");
+	
+    std::string app_path;
+
+    GetDiscordPath(app_path);
+
+    log("Successfully Installed!", 2);
+    std::cout << "Press Enter to exit...";
+    std::cin.ignore();
+    exit(EXIT_SUCCESS);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void GetDiscordPath(std::string &app_path)
+{
+    char* appdataPATH = nullptr;
+    size_t appdata_buffer = 0;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    // Locate AppData\Local path
+    if (_dupenv_s(&appdataPATH, &appdata_buffer, "LOCALAPPDATA") == 0 && appdataPATH != nullptr)
+    {
+        app_path = (std::string)appdataPATH;
+        free(appdataPATH);
+    }
+    else
+    {
+        log("Failed to locate variable pointing to AppData\\Local", 0);
+        exit(EXIT_FAILURE);
+    }
+	
+    // Check if discord canary folder is present
+    if (IsDiscordInstalled(app_path) == 1)
+    {
+        app_path += "\\DiscordCanary";
+    }
+    else 
+        exit(EXIT_FAILURE);
+}
+
+void GlasscordDownload(std::string &app_path)
+{
+    log("Downloading glasscord.asar from github repository...", 2);
+    LPCWSTR download_url = L"https://github.com/AryToNeX/Glasscord/releases/download/v1.0.1/glasscord.asar";
+    LPCWSTR download_path = (LPCWSTR)(app_path + "\\glasscord.asar").c_str();
+
+    URLDownloadToFile(NULL, download_url, download_path, 0, NULL);
+}
+
+int IsDiscordInstalled(std::string app_path)
+{
+    // Display appropiate message based on the folders found
+    if (std::filesystem::is_directory(app_path + "\\DiscordCanary"))
+    {
+        log("Discord Canary folder present", 2);
+        return 1;
+    }
+    else if (std::filesystem::is_directory(app_path + "\\Discord"))
+        log("Discord folder found but not Discord Canary. Ensure Canary is installed before installing Glasscord", 1);
+    else
+        log("Discord Canary folder not found. Ensure Canary is installed before installing Glasscord", 1);
+        
+    return 0;
+}
+
+void log(std::string msg, int msg_lvl)
+{
+	// Get local time in HH:MM:SS 24 hour format
+    using std::chrono::system_clock;
+    std::time_t tt = system_clock::to_time_t(system_clock::now());
+    struct std::tm* ptm = std::localtime(&tt);
+    std::cout << "[" << std::put_time(ptm, "%X") << "]";
+
+	// Display message based on msg_lvl code
+	switch(msg_lvl)
+	{
+		case 0:
+            eku::setcolor(eku::red, eku::defbackcol);
+            std::cout << "[ERROR]: " << msg << std::endl;
+            break;
+		case 1:
+            eku::setcolor(eku::yellow, eku::defbackcol);
+            std::cout << "[WARNING]: " << msg << std::endl;
+            break;
+		case 2:
+            std::cout << "[INFO]: " << msg << std::endl;
+            break;
+	}
+    eku::setcolor(eku::deftextcol, eku::defbackcol);
+}
