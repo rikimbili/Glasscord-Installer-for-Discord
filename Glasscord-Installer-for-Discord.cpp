b6c804bd67088108280d3ab53c663640bd6a8d64
Raciel <39206666+Rikimbili@users.noreply.github.com>
2021-07-11 01:16:36 -0400

    Semi-finalized wip version. Only check running discord process left to do

--- Glasscord-Installer-for-Discord.cpp
+++ Glasscord-Installer-for-Discord.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <regex>
 #include <Windows.h>
 #include <filesystem>
 #include <ctime>    
 #include <iomanip>
 #include <chrono>       
-#include "concol.h"
+#include "concol.h" // Custom library for terminal color customization: fonts, background, etc
 
 #pragma comment(lib, "urlmon.lib")
 
+void CheckDiscordRunning(); // Checks if Discord Canary is running in the background, not implemented
 void GetDiscordPath(std::string& app_path); // Gets discord path if it exists
 void GetAppPath(std::string& app_path); // Gets latest app path inside of discord folder
 void GlasscordDownload(std::string app_path); // Download glasscord.asar to discordcanary app directory
+void JsonFileIO(std::string& app_path); // Perform checks and I/O operations on app directory's json files
 int IsDiscordFound(std::string app_path); // Returns 1 if Discord Canary is installed, returns 0 otherwise
 void log(std::string, int message_lvl); //Log message in console with a message level > Level 0: Error; Level 1: Warning; Level 2: Info
 
@@ -21,20 +25,25 @@ int main()
     eku::concolinit();
     SetConsoleTitle(L"Glasscord Installer for Discord by Rikimbili");
 	
+    // Check if there's an instance of Discord Running and only continue after it has been closed.
+    CheckDiscordRunning();
+
+    // Obtaining Discord app path and downloading glasscord 
     std::string app_path;
-
     GetDiscordPath(app_path);
-
     GetAppPath(app_path);
-    app_path += "\\resources\\app";
+    app_path += "\\resources\\app\\backup";
     if (!std::filesystem::is_directory(app_path))
     {
         log("Empty or non-existent directory. Discord may have changed their folder structure. Check Github for updates", 0);
         exit(EXIT_FAILURE);
     }
-
     GlasscordDownload(app_path);
 
+    // Operations and checks of the json file in the app dir
+    JsonFileIO(app_path);
+
+    // If this line is reached, then it is/was installed successfully 
     log("Successfully Installed!", 2);
     std::cout << "Press Enter to exit...";
     std::cin.ignore();
@@ -101,11 +110,48 @@ void GlasscordDownload(std::string app_path)
     // Check if download was successful
     if (FAILED(hr))
     {
-        log("Could not download glasscord.asar. Make sure Discord is fully closed and try again", 0);
+        log("Could not download glasscord.asar to app directory. Make sure Discord is fully closed and try again", 0);
         exit(EXIT_FAILURE);
     }
     else
         log("glasscord.asar successfully downloaded", 2);
+}
+
+void JsonFileIO(std::string& app_path)
+{
+	std::fstream f_existing;
+	std::ofstream f_new;
+	std::string json_data;
+
+	// Copy package.json content to json_data if file exists
+	if (!std::filesystem::exists(app_path + "\\package.json"))
+	{
+		log("No package.json file exists in app directory. A Discord reinstall/update might fix this issue", 0);
+		exit(EXIT_FAILURE);
+	}
+	f_existing.open(app_path + "\\package.json", std::ios::in);
+	f_existing >> json_data;
+	f_existing.close();
+	// Copy json_data to the created package.original.json file if it doesn't exist
+	if (!std::filesystem::exists(app_path + "\\package.original.json"))
+	{
+		f_new.open(app_path + "\\package.original.json", std::ios::out);
+		f_new << json_data;
+		f_new.close();
+	}
+	else
+		log("package.original.json already exists in app directory", 2);
+
+	// Replace index.js for glasscord.asar in package.json
+	if (strstr(json_data.c_str(), "index.js"))
+	{
+		json_data = std::regex_replace(json_data, std::regex("index.js"), "glasscord.asar");
+		f_existing.open(app_path + "\\package.json", std::ios::out);
+		f_existing << json_data;
+		f_existing.close();
+	}
+	else
+		log("package.json already has glasscord.asar as main argument", 2);
 }
 
 int IsDiscordFound(std::string app_path)
